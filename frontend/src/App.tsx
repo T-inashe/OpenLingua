471b3f19d3414297d4e6dd4dbe08d8926e3f6de5
T-inashe <2588966@students.wits.ac.za>
2025-08-17 09:11:31 +0200

    updated gitignore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 // import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/landingPage';
+import UserForm from './components/userForm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<UserForm/>}/>
+        <Route path="/landingPage" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
